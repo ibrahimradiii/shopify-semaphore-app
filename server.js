275270e08b36fc113723ec5bea0a1769cfc4a4d6
Ibrahim Radi <ibrahimradi@Ibrahims-MacBook-Air.local>
2024-06-10 04:13:09 +0800

    Fixed view paths and ensured correct structure

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 require('dotenv').config();
 const { v4: uuidv4 } = require('uuid');
 
